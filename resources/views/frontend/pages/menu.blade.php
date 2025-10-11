097528476ef84a185ae2a4b00f9155004ca36761
Razia Haque <raziahaque1990@gmail.com>
2025-10-11 22:27:14 +0600

    change cover photo of menu page

--- resources/views/frontend/pages/menu.blade.php
+++ resources/views/frontend/pages/menu.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <section class="home-slider owl-carousel">
-        <div class="slider-item" style="background-image: url({{ asset('frontend/assets/images/dessert-4.jpg') }});background-size: cover; background-position: center; background-repeat: no-repeat;" data-stellar-background-ratio="0.5">
+        <div class="slider-item" style="background-image: url({{ asset('frontend/assets/images/biriyani.jpg') }});background-size: cover; background-position: center; background-repeat: no-repeat;" data-stellar-background-ratio="0.5">
             <div class="overlay"></div>
             <div class="container">
                 <div class="row slider-text justify-content-center align-items-center">
